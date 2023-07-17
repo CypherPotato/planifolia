b059e5bab96f0d9c1422c588866f9f5a6ec0e3bd
CypherPotato <test@gmail.com>
2023-07-17 14:45:33 -0300

    fix var name

--- src/internal/resolveFIle.js
+++ src/internal/resolveFIle.js
@@ -2,5 +2,5 @@ window.Planifolia.resolveFile = (partialName) => {
     var basePath = window.PlanifoliaSettings.basePath;
     var prefix = window.location.origin + window.location.pathname;
     var result = window.Planifolia.joinPaths(prefix, basePath, partialName + ".html");
-    return window.PlanifoliaSettings.onFileResolve(result);
+    return window.PlanifoliaSettings.handleFileResolve(result);
 }
